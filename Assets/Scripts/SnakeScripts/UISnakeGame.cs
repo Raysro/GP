5dc7dc1adedabe2cac43e67c4cf9a843bb7df45b
Abdar21 <ramasf21@gmail.com>
2023-01-02 16:36:44 +0300

    add add

--- Assets/Scripts/SnakeScripts/UISnakeGame.cs
+++ Assets/Scripts/SnakeScripts/UISnakeGame.cs
@@ -6,43 +6,12 @@ using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 public class UISnakeGame : MonoBehaviour
 {
-    public GameObject[] boards;
-    public GameObject[] Warning;
     public Text scoreTXT;
     public int score = 0;
     private void Update()
     {
         scoreTXT.text = "SCORE: " + score;
-        if (score == 40)
-        {
-            Warning[0].SetActive(true);
-            Warning[1].SetActive(true);
-        }
-        if (score == 50)
-        {
-            boards[0].SetActive(true);
-            boards[1].SetActive(true);
-        }
-        if (score == 120)
-        {
-            Warning[2].SetActive(true);
-            Warning[3].SetActive(true);
-        }
-        if (score == 130)
-        {
-            boards[2].SetActive(true);
-            boards[3].SetActive(true);
-        }
-        if (score == 200)
-        {
-            Warning[4].SetActive(true);
-            Warning[5].SetActive(true);
-        }
-        if (score == 210)
-        {
-            boards[4].SetActive(true);
-            boards[5].SetActive(true);
-        }
+        
     }
     public void UpdateScore()
     {
