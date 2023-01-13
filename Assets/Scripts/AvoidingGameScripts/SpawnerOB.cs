caeaf1a26b35313c962734fa7aac288c49a2f04c
Abdar21 <ramasf21@gmail.com>
2023-01-13 19:36:29 +0300

    Add Add

--- Assets/Scripts/AvoidingGameScripts/SpawnerOB.cs
+++ Assets/Scripts/AvoidingGameScripts/SpawnerOB.cs
@@ -8,13 +8,40 @@ public class SpawnerOB : MonoBehaviour
     void Start()
     {
         StartCoroutine(spawnOB1());
+        StartCoroutine(spawnOB2());
+        StartCoroutine(spawnOB4());
     }
     private IEnumerator spawnOB1()
     {
         while (true)
         {
-            yield return new WaitForSeconds(3f);
-            Instantiate(Obs[Random.Range(0, 3)],transform.position=new Vector3(Random.Range(-7f,7f),15f,0f),transform.rotation);
+            yield return new WaitForSeconds(2f);
+            Instantiate(Obs[0],transform.position=new Vector3(Random.Range(-7f,7f),15f,0f),transform.rotation);
+        }
+    }
+    private IEnumerator spawnOB2()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            Instantiate(Obs[1], transform.position = new Vector3(Random.Range(-7f, 7f), 15f, 0f), transform.rotation);
+        }
+    }
+    private IEnumerator spawnOB3()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(4f);
+            Instantiate(Obs[2], transform.position = new Vector3(Random.Range(-7f, 7f), 15f, 0f), transform.rotation);
+        }
+    }
+    private IEnumerator spawnOB4()
+    {
+        while (true)
+        {
+            Instantiate(Obs[3], transform.position=new Vector3(8.732107f,15f,0f), transform.rotation);
+            yield return new WaitForSeconds(2f);
+            
         }
     }
 }
